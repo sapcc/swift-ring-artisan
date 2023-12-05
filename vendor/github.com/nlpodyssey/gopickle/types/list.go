f9ec96cf8add6d7286a7dc809ca4a6b96a1821f6
VoigtS <s.voigt@sap.com>
2023-12-05 10:55:37 +0100

    Bump GoPickle

--- vendor/github.com/nlpodyssey/gopickle/types/list.go
+++ vendor/github.com/nlpodyssey/gopickle/types/list.go
@@ -4,6 +4,11 @@
 
 package types
 
+import (
+	"fmt"
+	"strings"
+)
+
 // ListAppender is implemented by any value that exhibits a list-like
 // behaviour, allowing arbitrary values to be appended.
 type ListAppender interface {
@@ -46,4 +51,30 @@ func (l *List) Get(i int) interface{} {
 // Len returns the length of the List.
 func (l *List) Len() int {
 	return len(*l)
+}
+
+func (*List) Call(args ...interface{}) (interface{}, error) {
+	if len(args) == 0 {
+		return NewList(), nil
+	}
+	if len(args) == 1 {
+		return args[0], nil
+	}
+	return nil, fmt.Errorf("List: invalid arguments: %#v", args)
+}
+
+func (l *List) String() string {
+	if l == nil {
+		return "nil"
+	}
+	o := new(strings.Builder)
+	o.WriteString("[")
+	for i, v := range *l {
+		if i > 0 {
+			o.WriteString(", ")
+		}
+		fmt.Fprintf(o, "%v", v)
+	}
+	o.WriteString("]")
+	return o.String()
 }
